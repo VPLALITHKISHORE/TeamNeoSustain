836dd59e469ccb0b46fd1b1d4f70fba4cf3013b6
keerthivasanA01 <akeerthi620@gmail.com>
2025-07-25 21:40:12 +0530

    Layout_update

--- user_inferface/src/components/Layout/Layout.tsx
+++ user_inferface/src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Sidebar from './Sidebar';
 import './Layout.css';
 
@@ -7,24 +7,8 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  // Add chat bot script
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://app.wonderchat.io/scripts/wonderchat.js';
-    script.setAttribute('data-name', 'wonderchat');
-    script.setAttribute('data-address', 'app.wonderchat.io');
-    script.setAttribute('data-id', 'cmas3uwfw011evvezmy0qmhik');
-    script.setAttribute('data-widget-size', 'normal');
-    script.setAttribute('data-widget-button-size', 'normal');
-    script.defer = true;
-    
-    document.body.appendChild(script);
-    
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-  
+  // Removed chat bot script
+
   return (
     <div className="layout">
       <Sidebar />
